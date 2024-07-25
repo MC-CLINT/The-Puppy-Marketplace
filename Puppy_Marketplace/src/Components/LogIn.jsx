052e1e629bb46c923fb6f10aa40f445437f421ef
Frank Gbadago <slymawuli3@gmail.com>
2024-07-25 13:25:48 +0000

    changes to the lohIn page

--- Puppy_Marketplace/src/Components/LogIn.jsx
+++ Puppy_Marketplace/src/Components/LogIn.jsx
@@ -16,38 +16,37 @@ export default function LogIn() {
               <div className="flex flex-row justify-between">
 
                 <div>
-                <img className='h-14 w-14' src={logo32}/>
+                <img className='h-[25px] w-[25px]' src={logo32}/>
                 </div>
             
                 <div>
-                <p className="text-xl font-extrabold">PUPPY</p>
-                <p className="text-xl font-extrabold">MARKETPLACE</p>
+                <p className="text-sm font-extrabold">PUPPY</p>
+                <p className="text-sm font-extrabold">MARKETPLACE</p>
                 </div>
 
                 <div>
-                <img  className='h-14 w-14' src={logo32}/>
+                <img  className='h-[25px] w-[25px]' src={logo32}/>
                 </div>
 
               </div>
 
               <p className="text-yellow-400">...................................................</p>
-              <p className="text-lg font-normal leading-7">Diverse Marketplace For Dog Enthusiasts</p>
-              <p className="text-lg font-normal leading-7">- Connect with buyers and sellers of puppies</p>
+              <p className="text-[10px] font-normal  ">Diverse Marketplace For Dog Enthusiasts</p>
+              <p className="text-[10px] font-normal "> Connect with buyers and sellers of puppies</p>
 
           </div>
 
           
-            <img src={dog}/>
+            <img src={dog} className='w-[230px] '/>
         
 
 
-          <div className="leading-4">
-            <p className="text-sm font-normal text-yellow-400">Facing any trouble ?</p>
-            <p className="text-sm font-normal text-yellow-400 mt-2">Contact Us:</p>
-            <p className="text-sm font-normal mt-2">Email: support@puppymarketplace.com</p>
-            <p className="text-sm font-normal mt-2">Phone: +1-800-PUPPY-SUPPORT</p>
-            <p className="text-sm font-normal mt-2">Help Center: Visit our Help Center for more resources</p>
-
+          <div className="">
+            <p className="text-[10px] font-normal text-yellow-400">Facing any trouble ?</p>
+            <p className="text-[10px] font-normal text-yellow-400 mt-2">Contact Us:</p>
+            <p className="text-[10px] font-normal mt-2">Email: support@puppymarketplace.com</p>
+            <p className="text-[10px] font-normal mt-2">Phone: +1-800-PUPPY-SUPPORT</p>
+            <p className="text-[10px] font-normal mt-2">Help Center: Visit our Help Center for more resources</p>
             <p className="mt-5">&#169; 2024</p>
           
           </div>
